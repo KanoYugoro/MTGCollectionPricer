c2940aa8ff70f0484b3dc82459a91bdbed0f15b2
Kano Yugoro <kanoyugoro@gmail.com>
2023-10-01 09:04:24 -0500

    fixup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from base_api import (Base_Api)
 
 if __name__ == '__main__':
-    api = Base_Api('https://www.mtggoldfish.com/sets','Sealed#paper'
+    api = Base_Api('https://www.mtggoldfish.com/sets','Sealed#paper')
     response = api.make_get_request('Wilds+of+Eldraine')
     tables = Base_Api.find_tables(response.text)
     parsed_tables = Base_Api.parse_table(tables[0], ['Card Num', 'Mana Cost', 'Rarity', 'Daily Δ', 'Daily %', 'Weekly Δ', 'Weekly %'])
