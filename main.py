e767e6f3b048049e67b3d3f53c826aeea2e0c931
Kano Yugoro <kanoyugoro@gmail.com>
2023-10-01 09:03:54 -0500

    Filter out unused headers, standardize strings

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from base_api import (Base_Api)
 
-if __name__ == "__main__":
-    api = Base_Api("https://www.mtggoldfish.com/sets","Sealed#paper")
-    response = api.make_get_request("Wilds+of+Eldraine")
+if __name__ == '__main__':
+    api = Base_Api('https://www.mtggoldfish.com/sets','Sealed#paper'
+    response = api.make_get_request('Wilds+of+Eldraine')
     tables = Base_Api.find_tables(response.text)
-    parsed_tables = Base_Api.parse_table(tables[0])
+    parsed_tables = Base_Api.parse_table(tables[0], ['Card Num', 'Mana Cost', 'Rarity', 'Daily Δ', 'Daily %', 'Weekly Δ', 'Weekly %'])
     print(parsed_tables)
