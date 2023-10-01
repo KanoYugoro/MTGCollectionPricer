8b4d83590c68b46ed3e5bc6fe5bcf4fb9af33521
Kano Yugoro <kanoyugoro@gmail.com>
2023-10-01 08:59:15 -0500

    add base table parse

--- base_api.py
+++ base_api.py
@@ -1,5 +1,6 @@
 import requests
 import re
+from lxml import etree
 
 class Base_Api:
     def __init__(self, baseURL = "", postFix = ""):
@@ -16,7 +17,34 @@ class Base_Api:
         return response
     
     def find_tables(responseText):
-        # If I was doing heavier HTML operations, I'd use a dedicated HTML parser
         tables = []
         tables = re.findall("(<table.*?>.*?</table>)", responseText)
-        return tables
+        parsed_tables = []
+        for table in tables:
+            parsed_tables.append(etree.HTML(table).find("body/table"))
+        return parsed_tables
+
+    def parse_table(table, filterHeaders = []):
+        finalTable = []
+
+        header = table.find("thead/tr")
+        headerItems = iter(header)
+        columns = []
+        for item in headerItems:
+            columns.append(item.text)
+        
+        bodyRows = table.findall("tbody/tr")
+        for row in bodyRows:
+            finalRow = {}
+            itemCount = 0
+            for item in row:
+                if columns[itemCount] not in filterHeaders:
+                    if (columns[itemCount]) == "Card":
+                        finalRow[columns[itemCount]] = item.findtext("span/a")
+                    else:
+                        finalRow[columns[itemCount]] = item.text
+                itemCount += 1
+            finalTable.append(finalRow)
+
+        return finalTable
+                
